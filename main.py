cde717ac5e15423ab82b90137aef9ee1c0206cf7
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-11-18 17:48:42 +0100

    .

--- main.py
+++ main.py
@@ -861,14 +861,9 @@ elif options_sidebar == 'Machine Learning':
 elif options_sidebar == 'Object detection':
     from python_files import main_object_detection
 
-
 ###### S T O C K _ O V E R L A Y ##########
 elif options_sidebar == 'Stock market dashboard':
     from python_files import main_stock_overlay 
-
-
-
-
 
 elif options_sidebar == 'Contact':
     from python_files import main_contact 
