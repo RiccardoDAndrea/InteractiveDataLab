45a1092c03e941c50ac55f2f561be4c8d5492865
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-11-18 18:49:00 +0100

    add

--- python_files/main_contact.py
+++ python_files/main_contact.py
@@ -54,7 +54,7 @@ if 'Contact' in contact_possibilities:
             </p>
         ''', unsafe_allow_html=True)
         
-    if 'Vita' in contact_possibilities:
+if 'Vita' in contact_possibilities:
        
         langugage_version = st.radio('Which langauge do you want the CV', ['German version', 
                                                                            'English version'])
