403cbfc9ab91b059abb9919aacb32267e8317ec1
RiccardoDAndrea <riccardo.dandrea@live.de>
2023-12-31 17:16:45 +0100

    test

--- python_files/main_stock_overlay.py
+++ python_files/main_stock_overlay.py
@@ -22,99 +22,75 @@ def load_lottieurl(url:str):
         return None
     return r.json()
 
+
+
 ### START OF THE WEBPAGE ### 
 
 st.title('Stock Dashboard') 
 no_data_avaible_female = load_lottieurl('https://lottie.host/70333dae-5d9d-4887-ac38-25dcbfe23e80/3TCrl817lO.json')
-stock_options = st.multiselect(
-    'What are your Stocks',
-    options = ['AAPL', 'BYDDF', 'EONGY', 'LNVGF', 'NIO', 'PLUN.F', 'TSLA', 'TKA.DE', 'XIACF'])
 
-expander_df_stocks = st.expander("Do you want to see the stocks Data ?")
+stock_options = st.text_input("Enter your Stocks (comma-separated)")
+stock_options = [stock.strip() for stock in stock_options.split(',')]  # Teilen Sie die Eingabe am Komma und entfernen Sie Leerzeichen
 
-with expander_df_stocks:
-    if stock_options:
-        button_for_the_download = st.button('Download Data for the Stocks')
+
+if len(stock_options) == 0:
+    st.markdown("**you have nothing entered**")        
+    no_data_found = load_lottieurl('https://lottie.host/70333dae-5d9d-4887-ac38-25dcbfe23e80/3TCrl817lO.json')   
+    st_lottie( no_data_found,
+                quality='high',
+                width=650,
+                height=400)
+else :
+    
+    start_date, end_date = st.columns((1, 2))
+    
+    with start_date:
+        start_date_input = st.date_input("Start")
+    with end_date:
+        end_date_input = st.date_input("Last day")
+
+button_for_the_download = st.button('Download Data for the Stocks')
+if button_for_the_download:
+        progress_text = "Operation in progress. Please wait."
+        my_bar = st.progress(0, text=progress_text)
         
-        start_date, end_date = st.columns((1, 2))
-        
-        with start_date:
-            start_date_input = st.date_input("Start")
-        with end_date:
-            end_date_input = st.date_input("Last day")
-        
-        if button_for_the_download:
-            progress_text = "Operation in progress. Please wait."
-            my_bar = st.progress(0, text=progress_text)
+        close_df = pd.DataFrame()
+        for stock_option in stock_options:
+            data = yf.download(stock_option, start=start_date_input, end=end_date_input)
+            if 'Close' in data.columns:
+                close_df[stock_option] = data['Close']
+        if not close_df.empty:
+            close_df.reset_index(inplace=True)
+            close_df['Date'] = pd.to_datetime(close_df['Date']).dt.date
+            st.dataframe(close_df, hide_index=True)
             
-            close_df = pd.DataFrame()
-            for stock_option in stock_options:
-                data = yf.download(stock_option, start=start_date_input, end=end_date_input)
-                if 'Close' in data.columns:
-                    close_df[stock_option] = data['Close']
-            if not close_df.empty:
-                close_df.reset_index(inplace=True)  # Hinzufügen der ausgewählten Startdatum-Spalte
-                close_df['Date'] = pd.to_datetime(close_df['Date']).dt.date  # Hier wird nur das Datum extrahiert
-                st.dataframe(close_df, hide_index=True)
+            # Line Chart
+            st.markdown('## Line Chart')
+            line_chart = px.line(close_df, x='Date', y=stock_options, title='Stock Prices Over Time')
+            st.plotly_chart(line_chart)
+            
+            # P/E Ratio
+            st.markdown('## P/E Ratio')
+            pe_ratio_value = None
+            if len(stock_options) > 0:
+                # Get P/E ratio for the first stock option
+                try:
+                    stock_data = yf.Ticker(stock_options[0])
+                    pe_ratio_value = stock_data.info['trailingPE']
+                except Exception as e:
+                    st.warning(f"Error retrieving P/E ratio for {stock_options[0]}: {e}")
+            
+            if pe_ratio_value is not None:
+                st.metric(label="P/E Ratio", value=pe_ratio_value)
             else:
-                st.warning("No data available.")
-
-
-
-    if len(stock_options) == 0:
-        st.markdown("**you have nothing entered**")        
-        no_data_found = load_lottieurl('https://lottie.host/70333dae-5d9d-4887-ac38-25dcbfe23e80/3TCrl817lO.json')   
-        st_lottie( no_data_found,
-                    quality='high',
-                    width=650,
-                    height=400)
-        
-expander_charts = st.expander("Dashbaord")
-options_of_charts = st.multiselect(
-                    'What Graphs do you want?', ('Barchart', 
-                                                'Linechart', 
-                                                'Scatterchart', 
-                                                'Histogramm',
-                                                'Boxplot'))
-for chart_type in options_of_charts:
-
-    if chart_type == 'Histogramm':
-        st.write('You can freely choose your :blue[Histogramm]')
-        col1_col ,col2_bins = st.columns(2)
-        with col1_col:
-            x_axis_val_hist = st.selectbox('Select X-Axis Value', options=close_df.columns,
-                                        key='x_axis_hist_multiselect')
-        with col2_bins:
-            bin_size = st.slider('Bin Size', min_value=1, max_value=30, step=1, value=1, format='%d')
-        color = st.color_picker('Pick A Color')
-        hist_plot_1 = px.histogram(close_df, 
-                                    x=x_axis_val_hist, 
-                                    nbins=bin_size,
-                                    color_discrete_sequence=[color])
-        st.plotly_chart(hist_plot_1)
-        # Erstellen des Histogramms mit Plotly
-        fig = go.Figure(data=hist_plot_1)
-        st.divider()
-
-    elif chart_type == 'Linechart':
-        st.markdown('You can freely choose your :blue[Linechart] :chart_with_upwards_trend:')
-        col3, col4 = st.columns(2)
-        
-        # Hier können Sie eine Dropdown-Box hinzufügen, um die ausgewählte Aktienoption festzulegen
-        
-        # Überprüfen Sie, ob die ausgewählte Aktienoption im DataFrame vorhanden ist
-        if stock_option:
-            combined_data = pd.DataFrame({'Date': close_df['Date']})  # DataFrame für kombinierte Daten erstellen
-            if stock_option in close_df.columns:
-                combined_data[stock_option] = close_df[stock_option]
-            else:
-                st.warning(f"Selected stock option '{stock_option}' not found in the DataFrame.")
-
-            # Plotting the combined data
-            st.line_chart(data=combined_data.set_index('Date'), use_container_width=True)
-            st.divider()
+                st.warning("Unable to retrieve P/E ratio.")
         else:
-            st.warning("Please select at least one stock option.")
+            st.warning("No data available.")
+         
+
+ 
+            
+            
 
 
 
