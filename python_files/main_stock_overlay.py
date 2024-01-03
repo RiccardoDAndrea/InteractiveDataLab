646a8257419ed2a22be8c50bbe6ceb94c0305dfc
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-01-03 12:02:55 +0100

    news implemented

--- python_files/main_stock_overlay.py
+++ python_files/main_stock_overlay.py
@@ -2,10 +2,7 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 import yfinance as yf
 import pandas as pd
-import requests
 import plotly.express as px
-import plotly.io as pio
-import plotly.graph_objs as go
 import requests
 from bs4 import BeautifulSoup
 import newspaper
@@ -32,7 +29,9 @@ def get_quote_table(ticker):
     response = requests.get(url)
     
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 
+                             'html.parser')
+        
         tables = soup.find_all('table')
         
         if len(tables) >= 2:
@@ -45,20 +44,19 @@ def get_quote_table(ticker):
         print(f"Error: Unable to fetch data from {url}. Status code: {response.status_code}")
 
 
-
-
-
-
-
 ### START OF THE WEBPAGE ### 
 
 st.title('Stock Dashboard') 
-no_data_avaible_female = load_lottieurl('https://lottie.host/70333dae-5d9d-4887-ac38-25dcbfe23e80/3TCrl817lO.json')
+#no_data_avaible_female = load_lottieurl('https://lottie.host/70333dae-5d9d-4887-ac38-25dcbfe23e80/3TCrl817lO.json')
+
 info_text_stocks = st.expander('Information on how to find you Stocks ?')
 with info_text_stocks:
+
         st.info("""If you want to find your stock, go to https://de.finance.yahoo.com/ and enter only the ticker symbol of the 
                 stock on the Streamlit page. For example, Apple would be 'APPL'""", icon="ℹ️")
-stock_options = st.text_input("Enter your Stocks (comma-separated)",value='AAPL, TSLA')
+
+stock_options = st.text_input("Enter your Stocks (comma-separated)",
+                              value='AAPL, TSLA')
 stock_options = [stock.strip() for stock in stock_options.split(',')]  # Teilen Sie die Eingabe am Komma und entfernen Sie Leerzeichen
 
 
@@ -70,7 +68,6 @@ if len(stock_options) == 0:
                 width=650,
                 height=400)
 else :
-    
     start_date, end_date = st.columns((1, 2))
     
     with start_date:
@@ -81,37 +78,53 @@ else :
 button_for_the_download = st.button('Download Data for the Stocks')
 if button_for_the_download:
         progress_text = "Operation in progress. Please wait."
-        my_bar = st.progress(0, text=progress_text)
+        my_bar = st.progress(0, 
+                             text=progress_text)
         
         close_df = pd.DataFrame()
+
         for stock_option in stock_options:
-            data = yf.download(stock_option, start=start_date_input, end=end_date_input)
+            data = yf.download(stock_option, 
+                               start=start_date_input, 
+                               end=end_date_input)
+            
             if 'Close' in data.columns:
                 close_df[stock_option] = data['Close']
         if not close_df.empty:
             close_df.reset_index(inplace=True)
             close_df['Date'] = pd.to_datetime(close_df['Date']).dt.date
-            st.dataframe(close_df, hide_index=True, use_container_width=True)
+
+            st.dataframe(close_df, hide_index=True, 
+                         use_container_width=True)
             
             # Line Chart
             st.markdown('## Line Chart')
-            line_chart = px.line(close_df, x='Date', y=stock_options, title='Stock Prices Over Time')
+            line_chart = px.line(close_df, 
+                                 x='Date', 
+                                 y=stock_options, 
+                                 title='Stock Prices Over Time')
+            
             st.plotly_chart(line_chart)
                        
             # P/E Ratio and other metrics
             st.markdown('## Metrics')
             PE_ratio_col, dividends_col = st.columns(spec=(2,1))
+
             for stock_option in stock_options:
                 stock_info = get_quote_table(stock_option)
+
                 if stock_info:
                     PE_Ratio = stock_info.get('PE Ratio (TTM)', 'N/A')
                     with PE_ratio_col:
-                        st.metric(label=f"P/E Ratio ({stock_option})", value=PE_Ratio)
+                        st.metric(label=f"P/E Ratio ({stock_option})", 
+                                  value=PE_Ratio)
                     dividends_data = yf.Ticker(stock_option).dividends
+
                     with dividends_col:
                         if not dividends_data.empty:
                             last_dividend = str(dividends_data.iloc[-1])
-                            st.metric(label=f"Last Dividend ({stock_option}) in EUR", value=last_dividend)
+                            st.metric(label=f"Last Dividend ({stock_option}) in EUR", 
+                                      value=last_dividend)
                         else:
                             st.warning(f"No dividend data available for {stock_option}")
                 else:
@@ -122,38 +135,38 @@ if button_for_the_download:
 newspaper_expander = st.expander(label="News about your Stocks")
 
 with newspaper_expander:
+    stock_options = st.text_input("Enter your Stocks (comma-separated)", value='AAPL, TSLA', key="input_news")
+    stocks = [stock.strip() for stock in stock_options.split(',')]
 
-    # Lists to store text and summary for each stock
-    texts = []
-    summaries = []
-
-    for stock in stock_options:
-        st.header(f"News for {stock}")
-        url = f'https://finance.yahoo.com/quote/{stock}/news?p={stock}/'
-        
+    for stock_option in stocks:
+        st.header(f"News for {stock_option}")
+        url = f'https://finance.yahoo.com/quote/{stock_option}/'  # Ändern Sie dies entsprechend Ihrer Website oder Newsquelle
         article = newspaper.Article(url)
+       
         try:
             article.download()
             article.parse()
+            authors = article.authors
+            article_meta_data = article.meta_data
+            article_published_date = article_meta_data.get('article:published_time', 'N/A')
+            st.write("Authors:", ', '.join(authors))
+            st.write("Published Date:", article_published_date)
             article.nlp()
 
-            texts.append(article.text)
-            summaries.append(article.summary)
-            
-            st.divider()
+            tab1, tab2 = st.tabs(['Full Text', 'Summary'])
+            with tab1:
+                st.write(article.authors)
+                st.write(article_published_date)
+                st.write(article.text)
+
+            with tab2:
+                st.write(article.authors)
+                st.write(article.summary)
 
         except Exception as e:
-            st.error(f"Error processing news for {stock}: {e}")
+            st.error(f"Error processing news for {stock_option}: {e}")
 
-    # Displaying tabs for each stock
-    for i, stock in enumerate(stock):
-        st.header(f"News for {stock}")
-        tab1, tab2 = st.tabs([f'{stock} - Full Text', f'{stock} - Summary'])
-        with tab1:
-            st.write(texts[i])
-        with tab2:
-            st.write(summaries[i])
-        st.divider()
+
 
          
 
