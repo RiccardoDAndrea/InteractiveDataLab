1c9767693fdcd611262e31859f0b620c0e068b13
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-08-12 10:55:57 +0200

    Update navigation paths for pages

--- navigation/6_Face_detection.py
+++ navigation/6_Face_detection.py
@@ -1,8 +1,7 @@
 import streamlit as st
-import cv2
 import numpy as np
 from PIL import Image
-
+import cv2 
 # Load the cascade classifiers
 face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
