56a64bb839e2fb03a30c8a77856bf6d56f044bba
RiccardoDAndrea <riccardo.dandrea@live.de>
2025-02-18 10:57:54 +0100

    Add introductory blog content and enhance formatting in Blog_Post.py

--- navigation/Blog_Post.py
+++ navigation/Blog_Post.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write("Blog")
+st.write("""
+         # Blog
+         """)
+st.divider()
+st.markdown("""
+# Jeder Anfang ist schwer
+Ihr fangt ein neues Projekt an voller euphorie betriebt ihr euere Recherse und kauft alle notigen Komponeten weil ich voller tatendrang seit und los legenwollt. Wahrend der recherse stellt ihr fast
+es ist garnicht so schwer wie es aussieht. Und denkt euch **ich schaffe das**. Nun sitzt ihr dran musst Programmieren und stellt fest. Es ist irgendwie schwerere als erwartet. Was nun ? 
+Immer wenn wir was neues Lernen ist es anfangs schwer und frustrierend, das ist aber gut.
+Den daraus lernen wir. Daher motivieren ich jeden der neu Anfangt seine Skripte beizubehalten und 
+sie nicht zu loschen. Nach einer gewissen Zeit werde ihr darauf zuruck schauen und fragt euch warum habe ich es so programmiert es ware doch effizienter und sinnvoller hatte ich es so gemacht.
+Aber diese erleucht diesen optmiereungs prozess kommt erst mit der erfahrung. Wahrend die meisten ihre Anfangs projekte verwerfen weil sie sich schamen oder es nicht mehr anssehen vollen weil sie denken
+das sei viel zu schlecht. Rate ich diese beizubehalten und es als Erinnernung zunutzen wo ihre damals wart und wo ihr nun heute seit.
+            """)
